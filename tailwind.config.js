32031d1bf021b79c97deb5fdabbc23c98bf76d2e
phat <phatcute585@gmail.com>
2022-12-23 14:20:49 +0700

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-coffee' : '#C89F94'
+      }
+
+    },
   },
   plugins: [],
 }
